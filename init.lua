621d733a2b0cba531398f978f72aba6a3b933fc8
Feofanov Denis <omail4me@gmail.com>
2024-07-03 12:22:24 +0500

    Auto-sync Neovim config

--- init.lua
+++ init.lua
@@ -1,33 +1,43 @@
-   local ensure_packer = function()
-     local fn = vim.fn
-     local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-     if fn.empty(fn.glob(install_path)) > 0 then
-       fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-       vim.cmd [[packadd packer.nvim]]
-       return true
-     end
-     return false
-   end
+-- Auto-install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-   local packer_bootstrap = ensure_packer()
+local packer_bootstrap = ensure_packer()
 
 -- Plugin management with packer
-   require('packer').startup(function(use)
-     use 'wbthomason/packer.nvim'
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'ThePrimeagen/vim-be-good'
   use 'kylechui/nvim-surround'
-   end)
+  use 'gbprod/cutlass.nvim'
+  use 'gbprod/substitute.nvim'
+end)
 
-   -- Automatically set up your configuration after cloning packer.nvim
-   -- Put this at the end after all plugins
-   if packer_bootstrap then
-     require('packer').sync()
-   end
-   
 -- Configure nvim-surround
 require("nvim-surround").setup({
     -- Configuration options here
+})
+
+-- Configure cutlass
+require('cutlass').setup({
+  cut_key = 'x',
+  override_del = nil,
+  exclude = {},
+})
+
+-- Configure substitute
+require('substitute').setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
 })
 
 -- Set options
@@ -45,6 +55,12 @@ vim.keymap.set('n', '<leader>s', '<Cmd>call VSCodeCall("workbench.action.gotoSym
 vim.keymap.set('n', '<leader>w', '<Cmd>call VSCodeCall("workbench.action.showAllSymbols")<CR>')
 vim.keymap.set('n', '<leader>f', '<Cmd>call VSCodeCall("actions.find")<CR>')
 vim.keymap.set('n', '<leader>r', '<Cmd>call VSCodeCall("editor.action.startFindReplaceAction")<CR>')
+
+-- Substitute mappings
+vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
+vim.keymap.set("n", "ss", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
+vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
+vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
 
 vim.opt.clipboard:append("unnamedplus")
 
@@ -67,3 +83,7 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
   callback = auto_sync
 })
 
+-- Automatically set up your configuration after cloning packer.nvim
+if packer_bootstrap then
+  require('packer').sync()
+end

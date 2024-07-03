356cd709b73a2f06baa8c8ec0081b12f129fe1ce
Feofanov Denis <omail4me@gmail.com>
2024-07-03 13:18:23 +0500

    remove all packer auto-setup

--- init.lua
+++ init.lua
@@ -1,17 +1,3 @@
--- Auto-install packer if not installed
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-local packer_bootstrap = ensure_packer()
-
-
 -- Plugin management with packer
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -72,8 +58,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank({higroup = 'IncSearch', timeout = 200})
   end,
 })
-
--- Automatically set up your configuration after cloning packer.nvim
-if packer_bootstrap then
-  require('packer').sync()
-end

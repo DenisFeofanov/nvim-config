419e2ac7ebadcbfdd0a1e6c11195ee7295ad77bf
Denis <omail4me@gmail.com>
2024-07-04 08:56:33 +0500

    [chore] merge with old feats

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- Plugin management with packer
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'ThePrimeagen/vim-be-good'
-  use 'kylechui/nvim-surround'
-  use 'gbprod/cutlass.nvim'
-  use 'gbprod/substitute.nvim'
+    use 'wbthomason/packer.nvim'
+    use 'ThePrimeagen/vim-be-good'
+    use 'kylechui/nvim-surround'
+    use 'gbprod/cutlass.nvim'
+    use 'gbprod/substitute.nvim'
 end)
 
 -- Configure nvim-surround
@@ -14,24 +14,31 @@ require("nvim-surround").setup({
 
 -- Configure cutlass
 require('cutlass').setup({
-  cut_key = 'x',
-  override_del = nil,
-  exclude = {},
+    cut_key = 'x',
+    override_del = nil,
+    exclude = {}
 })
 
 -- Configure substitute
 require('substitute').setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
 })
 
 -- Set options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Key mappings
+
+vim.api.nvim_set_keymap('n', '<leader>c', ':lua open_nvim_config()<CR>', {
+    noremap = true,
+    silent = true
+})
 vim.keymap.set('n', '<leader>h', ':noh<CR>')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -43,18 +50,33 @@ vim.keymap.set('n', '<leader>f', '<Cmd>call VSCodeCall("actions.find")<CR>')
 vim.keymap.set('n', '<leader>r', '<Cmd>call VSCodeCall("editor.action.startFindReplaceAction")<CR>')
 
 -- Substitute mappings
-vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
-vim.keymap.set("n", "ss", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
-vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
-vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
+vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", {
+    noremap = true
+})
+vim.keymap.set("n", "ss", "<cmd>lua require('substitute').line()<cr>", {
+    noremap = true
+})
+vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", {
+    noremap = true
+})
+vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", {
+    noremap = true
+})
 
 vim.opt.clipboard:append("unnamedplus")
 
-
 -- Set up highlight for yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', {}),
-  callback = function()
-    vim.highlight.on_yank({higroup = 'IncSearch', timeout = 200})
-  end,
+    group = vim.api.nvim_create_augroup('highlight_yank', {}),
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 200
+        })
+    end
 })
+
+-- Function to open Neovim configuration
+_G.open_nvim_config = function()
+    vim.cmd('edit ~/.config/nvim/init.lua')
+end

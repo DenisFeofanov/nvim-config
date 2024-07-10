cf07a671764f0ab54dfdb227d3a995863dace575
Denis <omail4me@gmail.com>
2024-07-10 10:40:40 +0500

    [fix] plugins config executes only after packer install

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 -- Auto-install Packer if not present
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -32,33 +32,29 @@ vim.api.nvim_create_autocmd("User", {
     callback = function()
         vim.cmd "bw | silent! MasonUpdate" -- close packer window
         require("packer").compile()
-        vim.api.nvim_exec_autocmds("User", { pattern = "ConfigReady" })
-    end,
+        vim.api.nvim_exec_autocmds("User", {
+            pattern = "ConfigReady"
+        })
+    end
 })
 
--- Configure plugins after they're installed
-vim.api.nvim_create_autocmd("User", {
-    pattern = "ConfigReady",
-    callback = function()
-        -- Configure nvim-surround
-        require("nvim-surround").setup({
-            -- Configuration options here
-        })
+-- Configure nvim-surround
+require("nvim-surround").setup({
+    -- Configuration options here
+})
 
-        -- Configure cutlass
-        require('cutlass').setup({
-            cut_key = 'x',
-            override_del = nil,
-            exclude = {}
-        })
+-- Configure cutlass
+require('cutlass').setup({
+    cut_key = 'x',
+    override_del = nil,
+    exclude = {}
+})
 
-        -- Configure substitute
-        require('substitute').setup({
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        })
-    end,
+-- Configure substitute
+require('substitute').setup({
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
 })
 
 -- Set options
@@ -84,7 +80,10 @@ vim.keymap.set('n', '<leader>w', '<Cmd>call VSCodeCall("workbench.action.showAll
 vim.keymap.set('n', '<leader>f', '<Cmd>call VSCodeCall("actions.find")<CR>')
 vim.keymap.set('n', '<leader>r', '<Cmd>call VSCodeCall("editor.action.startFindReplaceAction")<CR>')
 vim.keymap.set('n', '<CR>', "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
-vim.api.nvim_set_keymap('n', '<leader>sync', ':lua sync_nvim_config()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sync', ':lua sync_nvim_config()<CR>', {
+    noremap = true,
+    silent = true
+})
 
 -- Substitute mappings
 vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", {
@@ -101,8 +100,6 @@ vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", {
 })
 
 vim.opt.clipboard:append("unnamedplus")
-   
-
 
 -- Set up highlight for yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -122,7 +119,8 @@ end
 
 -- Function to sync Neovim config
 _G.sync_nvim_config = function()
-    local handle = io.popen("cd ~/.config/nvim && git pull && git add . && git commit -m 'Auto-sync Neovim config' && git push")
+    local handle = io.popen(
+        "cd ~/.config/nvim && git pull && git add . && git commit -m 'Auto-sync Neovim config' && git push")
     if handle then
         local result = handle:read("*a")
         handle:close()
